541477ccce3d83d783b648a90c21ef20cf04fa88
freelancercoder9 <85793659+freelancercoder9@users.noreply.github.com>
2021-09-12 12:27:43 +0800

    updated forms for navigation

--- src/components/PreLogin.js
+++ src/components/PreLogin.js
@@ -1,30 +1,40 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 
 function PreLogin() {
+  const history = useHistory();
   return (
     <div className="border-2 border-black w-96 m-auto rounded-md mt-20">
       <div className="text-center text-2xl py-10">
-        <label
-          htmlFor="recipiApp"
-          className="font-serif font-semibold text-black"
-        >
+        <label htmlFor="recipiApp" className="font-serif font-semibold text-black">
           Recipe App
         </label>
       </div>
       <div className="flex justify-center py-5">
-        <NavLink
+        <button
           className="no-underline px-3 py-1 bg-green-500 text-white rounded-md"
-          to="/signIn"
+          onClick={() => {
+            history.push("/signIn");
+          }}
         >
+          {" "}
+          Sign In
+        </button>
+        <button
+          className=" no-underline px-3 py-1 bg-blue-500 text-white rounded-md ml-4"
+          onClick={() => {
+            history.push("/signUp");
+          }}
+        >
+          {" "}
+          Sign up
+        </button>
+        {/* <NavLink className="no-underline px-3 py-1 bg-green-500 text-white rounded-md" to="/signIn">
           Sign In
         </NavLink>
-        <NavLink
-          className="no-underline px-3 py-1 bg-blue-500 text-white rounded-md ml-4"
-          to="/signUp"
-        >
+        <NavLink className="no-underline px-3 py-1 bg-blue-500 text-white rounded-md ml-4" to="/signUp">
           Sign Up
-        </NavLink>
+        </NavLink> */}
       </div>
     </div>
   );
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,55 +1,58 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+
+import { NavLink, useHistory } from "react-router-dom";
+
 function SignIn() {
+  const history = useHistory();
+
+  const onClickSignIn = () => {
+    history.push("/dashboard");
+  };
   return (
-    <div className="border-2 border-black rounded-md w-6/12 m-auto mt-20">
-      <div className="text-center py-4">
-        <label htmlFor="signUp" className="text-2xl font-serif">
-          Sign In to your Account
-        </label>
-      </div>
-      <div className="mx-4">
-        <div className="flex justify-between py-3">
-          <label htmlFor="emailId" className="text-blue-900 font-sans text-xl">
-            Email ID
-          </label>
-          <input
-            type="text"
-            className=" border-2 border-gray-200 w-7/12 h-7 px-2 text-xl font-light"
-          />
-        </div>
-        <div className="flex justify-between py-3">
-          <label htmlFor="password" className="text-blue-900 font-sans text-xl">
-            Password
-          </label>
-          <input
-            type="text"
-            className=" border-2 border-gray-200 w-7/12 h-7 px-2 text-xl font-light"
-          />
-        </div>
-        <div className="py-3">
-          <label
-            htmlFor="forgotPwd"
-            className="font-serif  text-blue-800 text-xl"
-          >
-            Forgot Password ?
+    <form>
+      <div className="border-2 border-black rounded-md w-6/12 m-auto mt-20">
+        <div className="text-center py-4">
+          <label htmlFor="signUp" className="text-2xl font-serif">
+            Sign In to your Account
           </label>
         </div>
+        <div className="mx-4">
+          <div className="flex justify-between py-3">
+            <label htmlFor="emailId" className="text-blue-900 font-sans text-xl">
+              Email ID
+            </label>
+            <div className="w-7/12">
+              <input type="text" className=" border-2 border-gray-200 w-full h-7 px-2 text-xl font-light" />
+              <div style={{ color: "#cc0000" }}>{"please enter proper email"}</div>
+            </div>
+          </div>
+          <div className="flex justify-between py-3">
+            <label htmlFor="password" className="text-blue-900 font-sans text-xl">
+              Password
+            </label>
+            <div className="w-7/12">
+              <input type="text" className=" border-2 border-gray-200  w-full h-7 px-2 text-xl font-light" />
+              <div style={{ color: "#cc0000" }}>{"please enter valid password"}</div>
+            </div>
+          </div>
+          <div className="py-3">
+            <label htmlFor="forgotPwd" className="font-serif  text-blue-800 text-xl">
+              Forgot Password ?
+            </label>
+          </div>
+        </div>
+        <div className="flex justify-center my-3">
+          <button className="no-underline px-3 py-2 bg-blue-800 text-white rounded-md" onClick={onClickSignIn}>
+            Sign In
+          </button>
+        </div>
+        <div className="flex justify-center my-2">
+          <NavLink className=" text-black text-sm" to="/">
+            ← Back to Home
+          </NavLink>
+        </div>
       </div>
-      <div className="flex justify-center my-3">
-        <NavLink
-          className="no-underline px-3 py-2 bg-blue-800 text-white rounded-md"
-          to="/dashboard"
-        >
-          Sign In
-        </NavLink>
-      </div>
-      <div className="flex justify-center my-2">
-        <NavLink className=" text-black text-sm" to="/">
-          ← Back to Home
-        </NavLink>
-      </div>
-    </div>
+    </form>
   );
 }
 
